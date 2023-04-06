ba73545e0c02f2a20af3c432c6b645c02436b669
esra.emirli <esraemirli@outlook.com>
2023-04-06 23:17:47 +0300

    Create project

---
+++ app/src/main/java/com/emirli/rickandmorty/MainActivity.kt
@@ -0,0 +1,43 @@
+package com.emirli.rickandmorty
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.emirli.rickandmorty.ui.theme.RickAndMortyTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            RickAndMortyTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    MainScreen("Rick And Morty")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun MainScreen(name: String) {
+    Text(text = "Welcome to $name Application!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    RickAndMortyTheme {
+        MainScreen("Rick And Morty")
+    }
+}
