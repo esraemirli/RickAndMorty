d166be6f6fa8e332f6a0cadb8e3cc56982994f44
esra.emirli <esraemirli@outlook.com>
2023-04-08 13:32:36 +0300

    Implement splash screen and navigate to home screen

--- app/src/main/java/com/emirli/rickandmorty/ui/theme/Theme.kt
+++ app/src/main/java/com/emirli/rickandmorty/ui/theme/Theme.kt
@@ -1,21 +1,14 @@
 package com.emirli.rickandmorty.ui.theme
 
-import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material.MaterialTheme
-import androidx.compose.material.darkColors
 import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
 
-private val DarkColorPalette = darkColors(
-    primary = Purple200,
-    primaryVariant = Purple700,
-    secondary = Teal200
-)
-
-private val LightColorPalette = lightColors(
-    primary = Purple500,
-    primaryVariant = Purple700,
-    secondary = Teal200
+private val ColorPalette = lightColors(
+    primary = Green,
+    secondary = Red,
+    background = LightOrange,
+    surface = LightOrange
 
     /* Other default colors to override
     background = Color.White,
@@ -28,15 +21,9 @@ private val LightColorPalette = lightColors(
 )
 
 @Composable
-fun RickAndMortyTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composable () -> Unit) {
-    val colors = if (darkTheme) {
-        DarkColorPalette
-    } else {
-        LightColorPalette
-    }
-
+fun RickAndMortyTheme(content: @Composable () -> Unit) {
     MaterialTheme(
-        colors = colors,
+        colors = ColorPalette,
         typography = Typography,
         shapes = Shapes,
         content = content
